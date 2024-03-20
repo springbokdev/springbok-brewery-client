dc142a4c0ba6c59360db792f8f20753c48c4bd96
johnspangenberg <dev@springbok.space>
2024-03-20 17:01:38 +0100

    Initial commit

---
+++ src/test/java/space/springbok/breweryclient/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package space.springbok.breweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
